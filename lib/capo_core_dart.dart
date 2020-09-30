1120c2406b07b351b14126e2f6d4331976846d75
matteo <immatteoz@gmail.com>
2020-09-30 09:46:47 +0800

    删除grpc

--- lib/capo_core_dart.dart
+++ lib/capo_core_dart.dart
@@ -10,7 +10,3 @@ export 'src/wallet/keystore/rev_keystore.dart';
 export 'src/wallet/keystore/rev_mnemonic_keystore.dart';
 export 'src/wallet/wallet_manager.dart';
 export 'src/wallet/wallet_meta.dart';
-export 'src/rnode_grpc/rnode_grpc.dart';
-
-export 'src/generated_protoc_files/DeployServiceCommon.pb.dart';
-export 'src/generated_protoc_files/DeployServiceV1.pb.dart';
--- test/capo_core_dart_test.dart
+++ test/capo_core_dart_test.dart
@@ -1,15 +1,11 @@
 import 'package:bs58check/bs58check.dart';
 import 'package:capo_core_dart/capo_core_dart.dart';
-import 'package:capo_core_dart/src/generated_protoc_files/DeployServiceV1.pbgrpc.dart';
-import 'package:capo_core_dart/src/generated_protoc_files/RhoTypes.pb.dart';
-import 'package:capo_core_dart/src/rnode_grpc/rnode_grpc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:grpc/grpc.dart';
 import 'package:hex/hex.dart';
 import 'package:capo_core_dart/src/rev_address/rev_address.dart';
 import 'check_balance_rho.dart';
 import 'dart:convert';
-
 
 // _createClient() async {
 //     final trustedRoot = await rootBundle.load('assets/key/api.pem');
@@ -64,78 +60,76 @@ void main() async {
   test("test", () async {
     print("test");
 
-  // String address = RevAddress.getAddressFromEthAddress(ethAddress: "d32e794ab8441487d5155851fa2f1459c5802e0f");
-  // print("address: $address");
-  // return;
+    // String address = RevAddress.getAddressFromEthAddress(ethAddress: "d32e794ab8441487d5155851fa2f1459c5802e0f");
+    // print("address: $address");
+    // return;
 
-  Map valueMap = json.decode(keystoreString);
+    // Map valueMap = json.decode(keystoreString);
+    //
+    // REVMnemonicKeystore keystore = REVMnemonicKeystore.fromMap(valueMap);
+    // print("keystore: ${keystore.export()}");
+    // return;
 
-  REVMnemonicKeystore keystore = REVMnemonicKeystore.fromMap(valueMap);
-  print("keystore: ${keystore.export()}");
-  return;
+    // ClientChannel v_channel = ClientChannel(
+    //     "node2.root-shard.mainnet.rchain.coop",
+    //     port: 40401,
+    //     options: const ChannelOptions(
+    //         credentials: const ChannelCredentials.insecure()));
+    // final vService = DeployServiceClient(v_channel);
+    // String sigString = "304402201de355b1dfd456077b5d6ad203fe2581b06c46fa64c189173e9c2ad9e98180a802205f4aa15b3627768f2e26d4c6a615c214e55885b5b44f9827f8f85675b10bbae9";
+    // List<int> sig = HEX.decode(sigString);
+    // FindDeployQuery query = FindDeployQuery();
+    // query.deployId = sig;
 
-    
-      // ClientChannel v_channel = ClientChannel(
-      //     "node2.root-shard.mainnet.rchain.coop",
-      //     port: 40401,
-      //     options: const ChannelOptions(
-      //         credentials: const ChannelCredentials.insecure()));
-      // final vService = DeployServiceClient(v_channel);
-      // String sigString = "304402201de355b1dfd456077b5d6ad203fe2581b06c46fa64c189173e9c2ad9e98180a802205f4aa15b3627768f2e26d4c6a615c214e55885b5b44f9827f8f85675b10bbae9";
-      // List<int> sig = HEX.decode(sigString);
-      // FindDeployQuery query = FindDeployQuery();
-      // query.deployId = sig;
-  
-      // FindDeployResponse findDeployResponse = await vService.findDeploy(query);
-      // print("findDeployResponse: $findDeployResponse");
-  
-          ClientChannel observer_channel = ClientChannel(
-          "observer-asia.services.mainnet.rchain.coop",
-          port: 40401,
-          options: const ChannelOptions(
-              credentials: const ChannelCredentials.insecure()));
-      final deployService = DeployServiceClient(observer_channel);
-      var params = ExploratoryDeployQuery();
-      params.term = checkBalanceRho("11112mmfnqD3UgtpEAmFxVfU6g26W7fdyFgS6dNUpk9ycS2GXYU3pL");
-      final result = await deployService.exploratoryDeploy(params);
-      print("result:$result");
-  
-  
-  // RNodeGRPC grpc = RNodeGRPC(host: "node0.root-shard.mainnet.rchain.coop",port: 40401);
-  //   List<int> sig = HEX.decode("3044022051bad8d9b7ffe66a27d8fe3500003fc9c8d9e5854893c1b3d4d949802c19168102204a609f228a84a52baeb98be9d2519d6a6344a80cbaf789bde7da422f823475d1");
-  //   GDeployId gDeployId = GDeployId();
-  //   gDeployId.sig = sig;
-  //   GUnforgeable gUnforgeable = GUnforgeable();
-  //   gUnforgeable.gDeployIdBody = gDeployId;
-  //   Par par = Par();
-  //   par.unforgeables.add(gUnforgeable);
-  //   DataAtNameQuery dataAtNameQuery = DataAtNameQuery();
-  //   dataAtNameQuery.name = par;
-  //   dataAtNameQuery.depth = 1000;
-  //   ListeningNameDataResponse listeningNameDataResponse = await grpc.deployService.listenForDataAtName(dataAtNameQuery);
-  //   print("listeningNameDataResponse:${listeningNameDataResponse.payload}");
-  
-      // RNodeGRPC grpc = RNodeGRPC(host: "node0.root-shard.mainnet.rchain.coop",port: 40401);
-      // String sigString = "3044022079dfb8bae214dc9c47809ed5634da02e88681d36507048fee31458c4205eeda2022057028a2239ef96f21b5825e5956f48e8a4ebf2cb685c71862e845f6f9519a87b";
-      // List<int> sig = HEX.decode(sigString);
-      // FindDeployQuery query = FindDeployQuery();
-      // query.deployId = sig;
-  
-      // FindDeployResponse findDeployResponse = await grpc.deployService.findDeploy(query);
-      // String blockHash = findDeployResponse.blockInfo.blockHash;
-      // BlockQuery blockQuery = BlockQuery();
-      // blockQuery.hash = blockHash;
-      // BlockResponse blockResponse = await grpc.deployService.getBlock(blockQuery);
-  
-      // final cost = blockResponse.blockInfo.deploys.firstWhere((block){
-      //   return block.sig == sigString;
-      // });
-  
-      // print('cost:$cost');
-  // IsFinalizedQuery finalizedQuery = IsFinalizedQuery();
-  // finalizedQuery.hash = blockHash;
-  //    IsFinalizedResponse  isFinalizedResponse = await grpc.deployService.isFinalized(finalizedQuery);
-  //    print("isFinalized:${isFinalizedResponse.isFinalized}");
-    });
-  }
-  
+    // FindDeployResponse findDeployResponse = await vService.findDeploy(query);
+    // print("findDeployResponse: $findDeployResponse");
+
+    // ClientChannel observer_channel = ClientChannel(
+    //     "observer-asia.services.mainnet.rchain.coop",
+    //     port: 40401,
+    //     options: const ChannelOptions(
+    //         credentials: const ChannelCredentials.insecure()));
+    // final deployService = DeployServiceClient(observer_channel);
+    // var params = ExploratoryDeployQuery();
+    // params.term = checkBalanceRho(
+    //     "11112mmfnqD3UgtpEAmFxVfU6g26W7fdyFgS6dNUpk9ycS2GXYU3pL");
+    // final result = await deployService.exploratoryDeploy(params);
+    // print("result:$result");
+
+    // RNodeGRPC grpc = RNodeGRPC(host: "node0.root-shard.mainnet.rchain.coop",port: 40401);
+    //   List<int> sig = HEX.decode("3044022051bad8d9b7ffe66a27d8fe3500003fc9c8d9e5854893c1b3d4d949802c19168102204a609f228a84a52baeb98be9d2519d6a6344a80cbaf789bde7da422f823475d1");
+    //   GDeployId gDeployId = GDeployId();
+    //   gDeployId.sig = sig;
+    //   GUnforgeable gUnforgeable = GUnforgeable();
+    //   gUnforgeable.gDeployIdBody = gDeployId;
+    //   Par par = Par();
+    //   par.unforgeables.add(gUnforgeable);
+    //   DataAtNameQuery dataAtNameQuery = DataAtNameQuery();
+    //   dataAtNameQuery.name = par;
+    //   dataAtNameQuery.depth = 1000;
+    //   ListeningNameDataResponse listeningNameDataResponse = await grpc.deployService.listenForDataAtName(dataAtNameQuery);
+    //   print("listeningNameDataResponse:${listeningNameDataResponse.payload}");
+
+    // RNodeGRPC grpc = RNodeGRPC(host: "node0.root-shard.mainnet.rchain.coop",port: 40401);
+    // String sigString = "3044022079dfb8bae214dc9c47809ed5634da02e88681d36507048fee31458c4205eeda2022057028a2239ef96f21b5825e5956f48e8a4ebf2cb685c71862e845f6f9519a87b";
+    // List<int> sig = HEX.decode(sigString);
+    // FindDeployQuery query = FindDeployQuery();
+    // query.deployId = sig;
+
+    // FindDeployResponse findDeployResponse = await grpc.deployService.findDeploy(query);
+    // String blockHash = findDeployResponse.blockInfo.blockHash;
+    // BlockQuery blockQuery = BlockQuery();
+    // blockQuery.hash = blockHash;
+    // BlockResponse blockResponse = await grpc.deployService.getBlock(blockQuery);
+
+    // final cost = blockResponse.blockInfo.deploys.firstWhere((block){
+    //   return block.sig == sigString;
+    // });
+
+    // print('cost:$cost');
+    // IsFinalizedQuery finalizedQuery = IsFinalizedQuery();
+    // finalizedQuery.hash = blockHash;
+    //    IsFinalizedResponse  isFinalizedResponse = await grpc.deployService.isFinalized(finalizedQuery);
+    //    print("isFinalized:${isFinalizedResponse.isFinalized}");
+  });
+}
